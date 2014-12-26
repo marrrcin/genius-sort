8f665ada66ef2fc610523974ea4fea7b85c1b20f
marrrcin <m.zablo@gmail.com>
2014-12-26 21:36:01 +0100

    Version v13.3 build 1232 optimized

---
+++ GeniusSort/Genius.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GeniusSort
+{
+    public static class Genius
+    {
+        public static void Sort(List<int> toSort)
+        {
+            var threads = new List<Thread>();
+            while (toSort.Count > 0)
+            {
+                var num = toSort.Last();
+                toSort.RemoveAt(toSort.Count-1);
+                var sorter = new Thread(() =>
+                {
+                    Thread.Sleep(num * 1000);
+                    Console.Write(num + " ");
+                });
+                threads.Add(sorter);
+                sorter.Start();
+            }
+            threads.ForEach(t=>t.Join());
+            Console.Write("done!");
+        }
+    }
+}
---
+++ GeniusSort/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeniusSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new List<int> {9,7,6,1,1,1,2,4,5,3};
+            Genius.Sort(array);
+            Console.ReadKey();
+        }
+    }
+}
